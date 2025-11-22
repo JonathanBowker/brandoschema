61f46a428b9a24d4b225d7bf1725a1167191a30e
Jonathan Bowker <j.b.bowker@gmail.com>
2025-11-22 21:51:40 +0000

    docs: update MkDocs site

--- main.py
+++
@@ -1,2 +0,0 @@
-def define_env(env):
-    env.variables['version'] = "1.5"
--- plugins/copy_well_known/mkdocs_copy_well_known/__init__.py
+++
@@ -1,5 +0,0 @@
-from mkdocs.plugins import BasePlugin
-
-class CopyWellKnownPlugin(BasePlugin):
-    def on_post_build(self, config):
-        print("✅ Plugin hook: copy-well-known triggered")
